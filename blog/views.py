4a682a6d7214f026ded8ad2760b36df9f2d0a2b8
s-bayat <sajjad.bayat11@gmail.com>
2023-11-27 16:07:32 +0330

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request,'blog/index.html')
+
+
+def posts(request):
+    pass
+
+
+def single_post(request):
+    pass
